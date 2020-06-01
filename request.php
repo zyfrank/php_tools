219170e399e2704000843658446ca2d5adafb406
zyfrank <zy_frank@hotmail.com>
2020-06-01 17:01:11 +0800

    Update request.php

--- request.php
+++ request.php
@@ -3,13 +3,13 @@
 
 date_default_timezone_set('UTC');
    $ip = $_SERVER['REMOTE_ADDR'];
-   $datex = date(DATE_RFC2822);
-   $header = "";
+   $datex = date('l jS \of F Y h:i:s A');
+   $header = "Header:";
    foreach (getallheaders() as $name => $value) {
        $header = $header + $name + ":" + $value + "-----"
    }
 
-   $record = $datex "  ---  " + $header + "\n";
+   $record = $datex +  "  ---  " + $header + "\n";
    $fp = fopen('access.log', 'a');
    fwrite($fp, $record);
    fclose($fp);
