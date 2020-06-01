24a57b69f9f7c21b5d55c317fb88bc70fd0c2d83
zyfrank <zy_frank@hotmail.com>
2020-06-01 16:57:20 +0800

    Update request.php

--- request.php
+++ request.php
@@ -1,12 +1,15 @@
 <?php
 
-date_default_timezone_set('Asia/Kolkata'); // Set your Time Zone Here
 
+date_default_timezone_set('UTC');
    $ip = $_SERVER['REMOTE_ADDR'];
-   $datex = date('Y/m/d');
-   $timex = date('G:i:s');
-   
-   $record = $datex + " " + $timex + " " + $ip;
+   $datex = date(DATE_RFC2822);
+   $header = "";
+   foreach (getallheaders() as $name => $value) {
+       $header = $header + $name + ":" + $value + "-----"
+   }
+
+   $record = $datex "  ---  " + $header + "\n";
    $fp = fopen('access.log', 'a');
    fwrite($fp, $record);
    fclose($fp);
