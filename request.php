7faa84e9ba2e129497a47713ad8759354f5df339
zyfrank <zy_frank@hotmail.com>
2020-06-01 17:08:54 +0800

    Update request.php

--- request.php
+++ request.php
@@ -1,12 +1,9 @@
 <?php
+   date_default_timezone_set('UTC');
+   $datex = date("Y-m-d");
 
 
-date_default_timezone_set('UTC');
-   $ip = $_SERVER['REMOTE_ADDR'];
-   $datex = date("F j, Y, g:i a");
-
-
-   $record = $datex + "\n";
+   $record = $datex + "\r\n";
    $fp = fopen('access.logs', 'a');
    fwrite($fp, $record);
    fclose($fp);
