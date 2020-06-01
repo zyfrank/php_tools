2c3ab83199f05e2d0c8a72130b02a2546c1bbc5b
zyfrank <zy_frank@hotmail.com>
2020-06-01 17:19:24 +0800

    Update request.php

--- request.php
+++ request.php
@@ -3,14 +3,18 @@
    $datex = date("Y-m-d");
 
 
-   $record = $datex + "\r\n";
+   $record = $datex . "\r\n";
+
+   file_put_contents('access.logs', $record, FILE_APPEND)
    $fp = fopen('access.logs', 'a');
    fwrite($fp, $record);
    fclose($fp);
 ?>
 
 <?php
-date_default_timezone_set("America/New_York");
+date_default_timezone_set('UTC');
+   $datex = date("Y-m-d");
 echo "Today is " . date("Y-m-d") . "<br>";
 echo "The time is " . date("h:i:sa");
+echo "haha is " . $datex
 ?>
