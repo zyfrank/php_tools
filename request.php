614becbb309e21b3dddf47c59748caf6d3123e53
zyfrank <zy_frank@hotmail.com>
2020-06-03 12:22:03 +0800

    Update request.php

--- request.php
+++ request.php
@@ -1,31 +1,17 @@
 <?php
    date_default_timezone_set('UTC');
-   $datex = date("Y-m-d");
 
    $headersStr = "";
    $headers =  getallheaders();
    foreach($headers as $key=>$val){
       $headersStr .= $key . ': ' . $val . '-----';
    }
-   file_put_contents('access.logs', $headersStr, FILE_APPEND)
-/*   $record = $datex . "\r\n";
-
+   $record =  date("Y-m-d") . '  ' . date("h:i:sa") . ':::::' . $headersStr;
    file_put_contents('access.logs', $record, FILE_APPEND)
-   $fp = fopen('access.logs', 'a');
-   fwrite($fp, $record);
-   fclose($fp);*/
 ?>
 
 <?php
-date_default_timezone_set('UTC');
-//$datex = date("Y-m-d");
-echo "Today is " . date("Y-m-d") . "<br>";
-echo "The time is " . date("h:i:sa") . "<br>";
-//echo  $datex . "<br>"
-$headersStr = "";
-$headers =  getallheaders();
-foreach($headers as $key=>$val){
-  $headersStr .= $key . ': ' . $val . '-----';
-}
-echo $headersStr . "<br>";
+   date_default_timezone_set('UTC');
+   echo "Today is " . date("Y-m-d") . "<br>";
+   echo "The time is " . date("h:i:sa") . "<br>";
 ?>
