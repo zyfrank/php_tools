7965ec58aafac76c7b186ae665b7d97cc76ddb98
zyfrank <zy_frank@hotmail.com>
2020-06-03 12:02:50 +0800

    Update request.php

--- request.php
+++ request.php
@@ -3,25 +3,23 @@
    $datex = date("Y-m-d");
 
 
-   $record = $datex . "\r\n";
+/*   $record = $datex . "\r\n";
 
-//   file_put_contents('access.logs', $record, FILE_APPEND)
+   file_put_contents('access.logs', $record, FILE_APPEND)
    $fp = fopen('access.logs', 'a');
    fwrite($fp, $record);
-   fclose($fp);
+   fclose($fp);*/
 ?>
 
 <?php
 date_default_timezone_set('UTC');
-   $datex = date("Y-m-d");
+//$datex = date("Y-m-d");
 echo "Today is " . date("Y-m-d") . "<br>";
 echo "The time is " . date("h:i:sa") . "<br>";
 echo  $datex . "<br>"
- $header = "Header:";
-   foreach (getallheaders() as $name => $value) {
-       $header = $header . $name . ":" . $value . "-----"
-   }
+//$header = "Header:";
+foreach (getallheaders() as $name => $value) {
+       $header .= $name . ":" . $value . "-----"
+}
 echo $header
-   
-   
 ?>
