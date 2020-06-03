3c23d5e0ce0e1de5cf713f304ec218c7125cb8b8
zyfrank <zy_frank@hotmail.com>
2020-06-03 12:07:23 +0800

    Update request.php

--- request.php
+++ request.php
@@ -17,9 +17,13 @@ date_default_timezone_set('UTC');
 echo "Today is " . date("Y-m-d") . "<br>";
 echo "The time is " . date("h:i:sa") . "<br>";
 //echo  $datex . "<br>"
-$header = "Header:";
+/*$header = "Header:";
 foreach (getallheaders() as $name => $value) {
        $header .= $name . ":" . $value . "-----"
 }
-echo $header 
+echo $header */
+$headers =  getallheaders();
+foreach($headers as $key=>$val){
+  echo $key . ': ' . $val . '<br>';
+}
 ?>
