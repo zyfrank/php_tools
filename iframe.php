8051d59ce90b6795d39a03baef13f8a7ae58178e
zyfrank <zy_frank@hotmail.com>
2020-06-04 14:47:08 +0800

    Update iframe.php

--- iframe.php
+++ iframe.php
@@ -1,8 +1,8 @@
 <?php
 htmlspecialchars($_GET["url"]) or die("No url provided"); 
 echo "embed " .  htmlspecialchars($_GET["url"]) . "!" . "<br>";
-echo "<iframe src=\"" .  htmlspecialchars($_GET["url"]) . "title=\"Target Iframe\"  width=\"300\"
-    height=\"200\"> </iframe>";
+echo "<iframe src=\"" .  htmlspecialchars($_GET["url"]) . "title=\"Target Iframe\"  width=\"900\"
+    height=\"600\"> </iframe>";
 ?>
 
 
